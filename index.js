a3b117c45d9626da3fd15752ecf72c3fa7775470
nick <nick@babblevoice.com>
2021-02-02 11:54:04 +0000

    Progress - we can semi-understand pidf and xpidf

--- index.js
+++ index.js
@@ -4,8 +4,8 @@
 const assert = require( "assert" )
 const events = require( "events" )
 
+const parseuri = require( "drachtio-srf" ).parseUri
 const digestauth = require( "drachtio-mw-digest-auth" )
-const regparser = require( "drachtio-mw-registration-parser" )
 
 const crypto = require( "crypto" )
 
@@ -61,7 +61,7 @@ class Presence {
           "headers": {
             "Event": "presence",
             "Expires": reg.expiresin,
-            "Accept": "application/dialog-info+xml, application/xpidf+xml"
+            "Accept": "application/dialog-info+xml, application/xpidf+xml, application/pidf+xml"
           }
         } ).then( ( dialog ) => {
 
@@ -69,15 +69,10 @@ class Presence {
 
           dialog.on( "notify", ( req, res ) => {
 
-
             da( req, res, () => {
-
-              console.log( "notify authed" )
-              res.send( 200 )
-
-              console.log( `Received NOTIFY for event ${req.get( 'Event' )}` )
-              if (req.body) {
-                console.log( `Received content of type ${req.get('Content-Type')}: ${req.body}` )
+              /* We are now authed */
+              if( this.parsepidfxml( req, res ) ) {
+                res.send( 200 )
               }
             } )
           } )
@@ -91,7 +86,6 @@ class Presence {
       this.options.registrar.on( "unregister", ( reg ) => {
 
       } )
-
     }
 
     this.options.srf.use( "subscribe", ( req, res ) => {
@@ -121,36 +115,86 @@ class Presence {
     this.options.srf.use( "publish", ( req, res, next ) => {
 
       if ( req.method !== "PUBLISH" ) return next()
-      return next()
-
 
       this.authdigest( req, res, () => {
-        if( "application/pidf+xml" === req.get( "Content-Type" ) ) {
+        if( this.parsepidfxml( req, res ) ) {
+          let ifmatch = req.get( "sip-if-match" )
 
-          console.log( req.get( "To" ) )
-          parseString( req.body, ( err, res ) => {
-
-            console.log( res.presence.tuple[ 0 ].status[ 0 ].basic[ 0 ] )
-            console.log( res.presence.tuple[ 0 ].note[ 0 ] )
-
+          res.send( 200, {
+            headers: {
+              "Expires": Math.min( req.get( "expires" ), 3600 ),
+              "SIP-ETag": crypto.randomBytes( 16 ).toString( "hex" )
+            }
           } )
         }
-
-        let ifmatch = req.get( "sip-if-match" )
-        /* find our existing PUBLISH event */
-
-        //if( undefined === ifmatch ) {
-          //ifmatch =
-        //}
-
-        res.send( 200, {
-          headers: {
-            "Expires": Math.min( req.get( "expires" ), 3600 ),
-            "SIP-ETag": crypto.randomBytes( 16 ).toString( "hex" )
-          }
-        } )
       } )
     } )
+
+    this.em = new events.EventEmitter()
+  }
+
+  on( ev, cb ) {
+    this.em.on( ev, cb )
+  }
+
+  /*
+    Not all clients set the To field correctly. So we have to use the auth
+    credentials instead which we can rely on.
+
+    req.authorization.username, req.authorization.realm
+  */
+  parsepidfxml( req, res ) {
+    let ct = req.get( "Content-Type" )
+    let toparts = parseuri( req.getParsedHeader( "To" ).uri )
+
+    let understood = false
+    parseString( req.body, ( err, res ) => {
+      if( undefined !== res ) {
+        if( "application/pidf+xml" === ct ) {
+
+          /*
+            Tested with Zoiper5
+            Notes, when the mouse leaves Zoiper (on Linux) we get a note of 'Away'
+            So, the previous note matters. If you set the status to Busy then we get
+            'Busy' followed by 'Away' (in seperate messages) as the mouse leaves
+            the application.
+          */
+
+          this.em.emit( "presence", {
+            "username": req.authorization.username,
+            "realm": req.authorization.realm,
+            "status": res.presence.tuple[ 0 ].status[ 0 ].basic[ 0 ],
+            "note": res.presence.tuple[ 0 ].note[ 0 ]
+          } )
+
+          understood = true
+
+        } else if ( "application/xpidf+xml" === ct ) {
+          /*
+            Tested with Polycom VVX 101 5.9.5.0614
+            status = 'open'
+            substatus= 'online'
+
+            When DND is enabled
+          */
+          this.em.emit( "presence", {
+            "username": req.authorization.username,
+            "realm": req.authorization.realm,
+            "status": res.presence.atom[ 0 ].address[ 0 ].status[ 0 ][ "$" ].status,
+            "note": res.presence.atom[ 0 ].address[ 0 ].msnsubstatus[ 0 ][ "$" ].substatus
+          } )
+
+          understood = true
+        }
+      }
+    } )
+
+    if( !understood ) {
+      res.send( 400 /* Bad request - or at least we don't understand it */ )
+      return false
+    }
+
+    return true
   }
 }
 
